674444ef86c8f2367fefb1cfbfd5cd6022e40f47
Saša Drmić <sasadrmic032@gmail.com>
2017-08-11 12:01:10 +0200

    Nav, cards, deleted bloat, svg

--- js/custom/typing_animation.js
+++ js/custom/typing_animation.js
@@ -3,7 +3,7 @@ var aText = new Array(
 "Let's start", 
 "a New Project"
 );
-var iSpeed = 100; // time delay of print out
+var iSpeed = 50; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
 var iScrollAt = 20; // start scrolling up at this many lines
