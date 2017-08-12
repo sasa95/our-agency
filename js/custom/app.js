cd00492432cd0c4f7ab409bbf6b601fcc0212ff7
Saša Drmić <sasadrmic032@gmail.com>
2017-08-12 16:46:35 +0200

    Nav

---
+++ js/custom/app.js
@@ -0,0 +1,29 @@
+var lastScrollTop = 0;
+ $(document).on('ready scroll',function() {
+ 	var st = $(this).scrollTop();
+ 	var $header = $('#main-nav');
+ 	var $div = $('#main-nav > div');
+ 	var $nav = $('#main-nav nav');
+ 	var $hero = $('.hero');
+
+ 	if (st > lastScrollTop){
+    	if($(document).scrollTop() > $hero.height()){
+    		$nav.addClass('nav--fixed').removeClass('nav--gone');
+    	}
+    	else {
+    		if($(document).scrollTop() > 50){
+    			$nav.removeClass('nav--fixed').addClass('nav--gone');
+    		}
+    	}
+   } else {
+      if($(document).scrollTop() > $hero.height()){
+    		$nav.removeClass('nav--gone');
+    	}
+    	else {
+    		$nav.removeClass('nav--fixed nav--gone');
+    	}
+   }
+    lastScrollTop = st;
+
+  });
+
--- js/custom/init.js
+++ js/custom/init.js
@@ -1,2 +1,15 @@
 // Initialize collapse button
-$(".button-collapse").sideNav();
+$(".button-collapse").sideNav({
+	closeOnClick: true,
+	draggable:false,
+	onOpen: function(el) { 
+		$('#main-nav nav').addClass('transparent-nav'); 
+		$('#main-nav .brand-logo').addClass('no-brand-logo');
+		$('body').addClass('body--fixed');
+	},
+	onClose: function(el) { 
+		$('#main-nav nav').removeClass('transparent-nav'); 
+		$('#main-nav .brand-logo').removeClass('no-brand-logo');
+		$('body').removeClass('body--fixed');
+	}
+});
